08272d49e0a01eef8aec477ef043b747dc33e92f
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-01-13 00:51:52 +0530

    Time: 19 ms (73.10%) | Memory: 10.7 MB (10.38%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
 const int mod = 1e9 + 7;
-
-int findall(int n, int k, int tar,vector<vector<int>>& dp)
+int dp[1001][1001];
+int findall(int n, int k, int tar)
 {
     if(n==0 and tar==0)return 1;
     if (n <0)return 0;
     if (dp[n][tar] != -1)
         return dp[n][tar];
-
     int ans = 0;
     for (int i = 1; i <= k; i++)
     {
         if (tar-i>=0)
         {
-            ans = (ans + findall(n - 1, k, tar-i,dp)) % mod;
+            ans = (ans + findall(n - 1, k, tar-i)) % mod;
         }
     }
-
     return dp[n][tar] = ans;
 }
 
 int numRollsToTarget(int n, int k, int target)
 {
-    vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+    memset(dp,-1,sizeof(dp));
+    // vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
     int cur = 0;
-    return findall(n, k, target, dp);
+    return findall(n, k, target);
 }
 
 
