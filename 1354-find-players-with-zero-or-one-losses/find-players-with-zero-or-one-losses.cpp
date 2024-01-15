6a785edfbdf94fe898a9f3750751764ae2bc4ec1
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-01-16 00:54:13 +0530

    Time: 832 ms (12.87%) | Memory: 215.6 MB (5.13%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        map<int,int>w,l,vis;
+       map<int,int>w,l,vis;
         vector<int>wi,lo;
         vector<vector<int>>res;
         for(auto it : matches)
@@ -12,21 +12,25 @@ public:
         for(auto it : w)
         {
             if(l.find(it.first)==l.end())wi.push_back(it.first);
-            else
-            {
-                if(l[it.first]==1)
-                {
-                    lo.push_back(it.first);
-                    vis[it.first]=1;
-                }
-            }
         }
         for(auto it : l)
         {
-            if(it.second==1 and vis[it.first]==0)lo.push_back(it.first);
+            if(it.second==1 and vis[it.first]==0)
+            {
+                 vis[it.first]=1;
+                 lo.push_back(it.first);
+            }
+        }
+        for(auto it : w)
+        {
+                if(l[it.first]==1 and vis[it.first]==0)
+                {
+                    lo.push_back(it.first);
+                   
+                }
         }
         res.push_back(wi);
-        sort(lo.begin(),lo.end());
+        // sort(lo.begin(),lo.end());
         res.push_back(lo);
         return res;
     }
