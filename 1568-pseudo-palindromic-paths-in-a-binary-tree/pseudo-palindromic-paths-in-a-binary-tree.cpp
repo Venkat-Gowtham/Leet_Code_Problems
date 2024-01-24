df688d2128301f52fd7202b0f232c629ea2dff94
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-01-24 22:30:52 +0530

    Time: 291 ms (42.89%) | Memory: 177.7 MB (98.93%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findall(TreeNode* root, map<int,int>&mp)
+    {
+        if(!root)
+        {
+            return 0;
+        }
+        if(!root->left and !root->right)
+        {
+            mp[root->val]++;
+            int f=0;
+            for(auto it : mp)
+            {
+                if(it.second%2)
+                {
+                    if(!f)f=1;
+                    else {mp[root->val]--;return 0;}
+                }
+            }
+             mp[root->val]--;
+            return 1;
+        }
+        mp[root->val]++;
+        int l=findall(root->left,mp);
+        int r=findall(root->right,mp);
+        mp[root->val]--;
+        return l+r;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        map<int,int>mp;
+       return findall(root,mp);
+    }
+};
