f5d70694e4294fc7a52283d4281789d1aaf67f04
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-04-19 00:39:21 +0530

    Time: 76 ms (51.27%) | Memory: 101.2 MB (29.64%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findd(vector<vector<int>>& grid , int &x,int i , int j){
+        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size()) return 1;
+        if(grid[i][j]==0)return 1;
+        if(grid[i][j]==-1)return 0;
+        // x+=3;
+        grid[i][j]=-1;
+      int l =  findd(grid,x,i+1,j);
+      int r=   findd(grid,x,i,j-1);
+      int z =   findd(grid,x,i-1,j);
+      int y =  findd(grid,x,i,j+1);
+        
+        return l+r+z+y;
+        grid[i][j]=1;
+        
+        
+    }
+    int islandPerimeter(vector<vector<int>>& grid) {
+        for(int i =0;i<grid.size();i++){
+            for(int j =0;j<grid[i].size();j++){
+                if(grid[i][j]==1){
+                    int x=0;
+                  return  findd(grid,x,i,j);
+                    return x;
+                }
+            }
+        }
+        return 0;
+    }
+};
