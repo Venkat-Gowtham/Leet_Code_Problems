034dedd89e21bb2dac238453541d95759ac7eb4c
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-06-02 16:02:16 +0530

    Time: 88 ms (92.59%) | Memory: 130.4 MB (81.29%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -6,20 +6,18 @@ public:
         cin.tie(NULL);
             int ind = 0 , upto = 0 , i=0;
             for(i =0;i<gas.size();i++){
-                if(upto+gas[i]-cost[i]<0){
+                upto+=gas[i]-cost[i];
+                if(upto<0){
                     upto=0;
                     ind = i+1;
-                }
-                else{
-                    upto+=gas[i]-cost[i];
                 }
             }
             i=0;
             while(i<ind){
-                if(upto+gas[i]-cost[i]<0){
+                upto+=gas[i]-cost[i];
+                if(upto<0){
                     return -1;
                 }
-                upto+=gas[i]-cost[i];
                 i++;
             }
             return ind;
