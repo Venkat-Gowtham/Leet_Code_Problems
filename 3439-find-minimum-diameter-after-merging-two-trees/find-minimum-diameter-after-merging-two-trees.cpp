c067976a6ace812752a9f0cf2c09b2aaf175ef95
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-07-17 11:47:04 +0530

    Time: 681 ms (73.54%) | Memory: 302.9 MB (72.80%) - LeetSync

--- 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
+++ 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,98 +1,63 @@
 class Solution {
 public:
-    vector<int>bfs(int start , vector<vector<int>>&adj,int n){
-      vector<int>distances(n+1,-1);
-      distances[start] = 0;
-      queue<int>q;
-      q.push(start);
-      while(!q.empty()){
-          int neighbor = q.front();
-          q.pop();
-          for(auto it : adj[neighbor]){
-              if(distances[it] == -1){
-                  distances[it] = distances[neighbor] +1;
-                  q.push(it);
-              }
-          }
-      }
-      return distances;
-  }
-  int findFarthest(vector<int>&dist){
-      int max_dist = -1;
-      int far_node = -1;
-      for(int i = 0;i<dist.size(); i++){
-          if(dist[i]>max_dist){
-              far_node = i;
-              max_dist = dist[i];
-          }
-      }
-      return far_node;
-  }
+    vector<int> bfs(int start, const vector<vector<int>>& adj) {
+        int n = adj.size();
+        vector<int> distances(n, -1);
+        distances[start] = 0;
+        queue<int> q;
+        q.push(start);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (int neighbor : adj[node]) {
+                if (distances[neighbor] == -1) {
+                    distances[neighbor] = distances[node] + 1;
+                    q.push(neighbor);
+                }
+            }
+        }
+        return distances;
+    }
+    
+    int findFarthest(const vector<int>& dist) {
+        int max_dist = -1;
+        int far_node = -1;
+        for (int i = 0; i < dist.size(); ++i) {
+            if (dist[i] > max_dist) {
+                max_dist = dist[i];
+                far_node = i;
+            }
+        }
+        return far_node;
+    }
+
+    int findDiameter(const vector<vector<int>>& adj) {
+        vector<int> dist = bfs(0, adj);
+        int farthest = findFarthest(dist);
+        dist = bfs(farthest, adj);
+        return *max_element(dist.begin(), dist.end());
+    }
+
     int minimumDiameterAfterMerge(vector<vector<int>>& tree1, vector<vector<int>>& tree2) {
-        int n = tree1.size(),m=tree2.size();
-        if(n<1 and  m<1)return 1;
-        if(n<1 or m<1)return 2;
-        // vector<int>adj1[n+1],adj2[m+1];
-        vector<vector<int>>adj1(n+1),adj2(m+1);
-        for(int i =0;i<tree1.size();i++){
-            adj1[tree1[i][0]].push_back(tree1[i][1]);
-            adj1[tree1[i][1]].push_back(tree1[i][0]);
-            // cout<<tree1[i][0]<<" "<<tree1[i][1]<<endl;
+        int n = tree1.size();
+        int m = tree2.size();
+        
+        if (n == 0 && m == 0) return 1;
+        if (n == 0 || m == 0) return 2;
+
+        vector<vector<int>> adj1(n + 1), adj2(m + 1);
+        for (const auto& edge : tree1) {
+            adj1[edge[0]].push_back(edge[1]);
+            adj1[edge[1]].push_back(edge[0]);
         }
-        for(int i = 0;i<tree2.size();i++){
-            adj2[tree2[i][0]].push_back(tree2[i][1]);
-            adj2[tree2[i][1]].push_back(tree2[i][0]);
-        }
-        // initial bfs call
-        vector<int> dist1 = bfs(0,adj1,n);
-        // finding farthest node 
-        int firstFarther = findFarthest(dist1);
-        
-        // finding distances from first fartther 
-        // using bfs
-        vector<int>ff1 = bfs(firstFarther,adj1,n);
-        
-        int secondFarther = findFarthest(ff1);
-        
-        // cout<<secondFarther<<" "<<ff1[secondFarther]<<endl;
-        vector<int>ss1 = bfs(secondFarther,adj1,n);
-        // cout<<"f- "<<firstFarther<<" "<<"s- "<<secondFarther<<endl;
-        vector<int>tree1_dist(n+1,0);
-        for(int i = 0 ;i<n+1;i++){
-            // cout<<ff1[i]<<" "<<ss1[i]<<endl;
-            tree1_dist[i] = max(ff1[i],ss1[i]);
-            cout<<i<<" "<<tree1_dist[i]<<endl;
+        for (const auto& edge : tree2) {
+            adj2[edge[0]].push_back(edge[1]);
+            adj2[edge[1]].push_back(edge[0]);
         }
         
-        // for tree 2 
-        // initial bfs call
-        vector<int> dist2 = bfs(0,adj2,m);
-        // finding farthest node 
-        int firstFarther1 = findFarthest(dist2);
+        int dia1 = findDiameter(adj1);
+        int dia2 = findDiameter(adj2);
         
-        // finding distances from first fartther 
-        // using bfs
-        vector<int>ff2 = bfs(firstFarther1,adj2,m);
-        
-        int secondFarther2 = findFarthest(ff2);
-        
-        vector<int>ss2 = bfs(secondFarther2,adj2,m);
-        
-        vector<int>tree2_dist(m+1);
-        for(int i = 0 ;i<m+1;i++){
-            tree2_dist[i] = max(ff2[i],ss2[i]);
-            // cout<<tree2_dist[i]<<" "<<i<<endl;
-        }
-        
-        vector<int>res(m);
-        int l = *min_element(tree1_dist.begin(),tree1_dist.end());
-        // cout<<l<<" ";
-        l+= *min_element(tree2_dist.begin(),tree2_dist.end());
-        // cout<<l<<" ";
-        int a = *max_element(tree1_dist.begin(),tree1_dist.end());
-        int b =*max_element(tree2_dist.begin(),tree2_dist.end());
-        // cout<<a<<" "<<b;
-        // ans+=*min_element(tree2_dist.begin(),tree2_dist.end());
-        return max({l+1,a,b});
+        return max({(dia1 + 1) / 2 + (dia2 + 1) / 2 + 1,dia1,dia2});
     }
 };
