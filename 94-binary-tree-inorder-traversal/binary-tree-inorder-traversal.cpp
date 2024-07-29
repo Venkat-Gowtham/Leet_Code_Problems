99ce5c0d35d62130cd58c166a97acb0cc3d9807d
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-07-29 11:53:32 +0530

    Time: 5 ms (7.51%) | Memory: 9.9 MB (33.67%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -12,31 +12,39 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        // morris algo for find inorder withot xtra space 
         vector<int>inorder;
-        stack<TreeNode*>st;
-        st.push(root);
-        if(!root)return {};
-        while(!st.empty()){
-            TreeNode *temp = st.top();
-            st.pop();
-            TreeNode * child = temp->left;
-            TreeNode *rc = temp->right;
-            if(!temp->left and !temp->right){
-            // if(temp->val == 1)cout<<"hl";
-                inorder.push_back(temp->val);
-                continue;
+        TreeNode * cur = root;
+        while(cur !=nullptr){
+            // if cur->left exists
+            if(cur->left){
+                // go for the rightmost child of cur->left
+                // and attach cur as its successor
+                TreeNode* predecessor = cur->left;
+                while(predecessor->right and predecessor->right!=cur){
+                    predecessor = predecessor->right;
+                }
+                // if pre->rigght not exists attach cur as its succsr
+                if(predecessor->right == nullptr){
+                    predecessor->right = cur;
+                    // go for the left sub tree of cur to do same
+                    cur = cur->left;
+                }
+                else{
+                    // already there's a conn to cur remove it 
+                    // to get the original tree struc
+                    predecessor->right = nullptr;
+                    // take cur ->node valu
+                    inorder.push_back(cur->val);
+                    // go for right of cur
+                    cur = cur->right;
+                }
             }
-            temp->left = nullptr;
-            temp->right = nullptr;
-            if(child)cout<<child->val;
-            if(!child)inorder.push_back(temp->val);
-            if(rc)st.push(rc);
-            if(child)st.push(temp);
-           if(child) st.push(child);
-            // while(child){ 
-            //     st.push(child);
-            //     child = child->left;
-            // }
+            else{
+                // if left sub doesnt exists take curVal and go for rig
+                inorder.push_back(cur->val);
+                cur = cur ->right;
+            }
         }
         return inorder;
     }
