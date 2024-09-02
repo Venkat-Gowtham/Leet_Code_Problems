104ae3208235fc42c2036f7040487b1473dbe869
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2024-09-02 14:31:55 +0530

    Time: 97 ms (52.44%) | Memory: 77 MB (22.40%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -5,12 +5,10 @@ public:
         for(int i = 0 ; i<chalk.size();i++){
             s += chalk[i];
         }
-        cout<<chalk.size()<<" "<<s;
         int  rem = 0;
         if(s>=k) rem = k; 
         else rem = k%s;
         if(rem == 0) return 0;
-        cout<<" "<<rem;
         for(int i = 0;i<chalk.size();i++){
             if(rem>=chalk[i]) rem -=chalk[i];
             else return i;
