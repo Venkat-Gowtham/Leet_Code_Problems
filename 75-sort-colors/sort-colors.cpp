c5ad489963553138b342c3dfee75e830dc79142d
Gowtham Varri <21P31A0542@acet.ac.in>
2025-01-07 23:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (47.12%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,55 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        if(nums.size()==1) return ;
-        if(nums.size()==2){
-            if(nums[0]>nums[1]) {swap(nums[0],nums[1]);}
-            return ;
-        }
-        int n = nums.size();
-        // 1 2 2 0
-        int i = 0, j = 1, k = n-1;
-        while(j<=k){
-            if(nums[i]==1){
-                if(nums[j]==1){
-                    j++;
-                }
-               else if(nums[j]==0){
-                    swap(nums[i],nums[j]);
-                     i++;
-                     j++;
-                }
-                else if(nums[j]==2){
-                    swap(nums[j],nums[k]);
-                    k--;
-                }
-                else if(nums[k]==0){
-                    swap(nums[j],nums[k]);
-                    swap(nums[i],nums[j]);
-                    k--;
-                    i++;
-                    j++;
-                }
-                else{
-                    i++;
-                    j++;
-                }
-               
+        int red=0,white=0,blue=nums.size()-1;
+
+        while(white<=blue){
+            if(nums[white]==0){
+                swap(nums[white],nums[red]);
+                red++;
+                white++;
             }
-            else if(nums[i]==2){
-                swap(nums[i],nums[k]);
-                if(nums[i]==0){
-                    i++;
-                    j++;
-                }
-                k--;
+            else if(nums[white]==1){
+                white++;
             }
             else {
-                i++;
-                j++;
+                swap(nums[white],nums[blue]);
+                blue--;
             }
         }
-
-        
     }
 };
