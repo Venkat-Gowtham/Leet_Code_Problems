ec600f2435c9f02be6764f11cd80c143eb46ed8f
Gowtham Varri <21P31A0542@acet.ac.in>
2025-01-08 22:58:01 +0530

    Time: 0 ms (100.00%) | Memory: 65.2 MB (36.58%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int ac=0,cr=0;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[abs(nums[i])]<0)return abs(nums[i]);
-            nums[abs(nums[i])]*=-1;
-
+        int slow = nums[0] , fast = nums[0];
+        do{
+            slow=nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-        return ac^cr;
-
+        return slow;
     }
 };
