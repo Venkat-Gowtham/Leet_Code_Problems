2fc321ffe683ab041e99b6a04fd9460a47674eb9
Gowtham Varri <21P31A0542@acet.ac.in>
2025-01-18 01:11:49 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (74.88%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        
+        int c =0;
+        while(num2){
+            c+=num2%2;
+            num2/=2;
+        }
+        cout<<c;
+        int res = 0;
+        for(int i = 31;i>=0;i--){
+            if((1<<i)&num1){
+                c--;
+                res|=(1<<i);
+                if(c==0)return res;
+            }
+        }
+      
+        for(int i=0;i<=31;i++){
+            if(!((1<<i)&num1)){
+                c--;
+            }
+            res=res|(1<<i);
+            if(c==0)return res;
+        }
+        return res;
+
+
+        
+
+    }
+};
