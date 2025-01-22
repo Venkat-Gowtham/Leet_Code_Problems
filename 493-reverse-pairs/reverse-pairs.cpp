0049ddcf058c501f244116c057f8bb197b8b94fd
Gowtham Varri <21P31A0542@acet.ac.in>
2025-01-22 12:02:17 +0530

    Time: 595 ms (6.72%) | Memory: 240.2 MB (55.21%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,60 +1,48 @@
 class Solution {
 public:
-int merge(vector<int>&nums, int low,int high)
-{
-    if(low==high)return 0;
-    int m=(low+(high-low)/2);
-   int res1= merge(nums,low,m);
-    int res2=merge(nums,m+1,high);
-    int y=low;
-   int  total=0,x=m+1;
-    while(y<=m)
-    {
-        while(x<=high)
-        {
-            long long val=1LL*2*nums[x];
-            if(nums[y]<=val)break;
-            x++;
-        }
-        total+=x-1-m;
-        y++;
-    }
-    vector<int>ans;
-    x=m+1;
-    y=low;
-    while(y<=m and x<=high)
-    {
-        if(nums[y]<=nums[x])
-        {
-            ans.push_back(nums[y]);
-            y++;
-        }
-        else
-        {
-            ans.push_back(nums[x]);
-            x++;
-        }
-    }
-    while(y<=m)
-    {
-         ans.push_back(nums[y]);
-         y++;
-    }
-    while(x<=high)
-    {
-         ans.push_back(nums[x]);
-         x++;
-    }
-    int k=0;
-    for(int i=low;i<=high;i++)
-    {
-        nums[i]=ans[k++];
-    }
-    return res1+res2+total;
-}
-    int reversePairs(vector<int>& nums) {
-        int n=nums.size();
-        return merge(nums,0,n-1);
+    int merge(vector<int>&nums , int l , int h ){
+        if(l == h) return 0;
+        int m = l+(h-l)/2;
+        int res1 = merge(nums,l,m);
+        int res2 = merge(nums,m+1,h);
+        int x = l , y = m+1, total=0;
+        while(x<=m){
+            while(y<=h){
 
+                long long val = 1LL*nums[y]*2;
+                if(nums[x]<=val) break;
+                y++;
+            }
+            total+=y-m-1;
+            x++;
+        }
+        vector<int>res;
+        x=l,y=m+1;
+        while(x<=m and y<=h){
+            if(nums[x]<=nums[y]){
+                res.push_back(nums[x]);
+                x++;
+            }
+            else{
+                res.push_back(nums[y]);
+                y++;
+            }
+        }
+        while(x<=m){
+            res.push_back(nums[x++]);
+        }
+        while(y<=h){
+            res.push_back(nums[y++]);
+        }
+        int k = 0;
+        for(int i = l;i<=h;i++){
+            nums[i] = res[k++];
+        }
+        return res1+res2+total;
+
+    }
+    int reversePairs(vector<int>& nums) {
+        
+        return merge(nums,0,nums.size()-1);
     }
 };
