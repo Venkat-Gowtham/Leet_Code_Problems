ecf1496283d0c2801831b0d3cf7b57dea49a16d9
Gowtham Varri <21P31A0542@acet.ac.in>
2025-01-24 18:31:41 +0530

    Time: 11 ms (72.06%) | Memory: 52.2 MB (84.49%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&adj , int node , vector<int>&vis,vector<int>&dp){
-        if(adj[node].size()==0){
-            return dp[node]=1;
+    int dfs(vector<vector<int>>& adj, int node, vector<int>& vis,
+            vector<int>& dp) {
+        if (adj[node].size() == 0) {
+            return dp[node] = 1;
         }
         vis[node] = 1;
         int cnt = 1;
-        for(auto it : adj[node]){
-           if(!vis[it]){
-                dp[it]=dfs(adj,it,vis,dp);
-                if(!dp[it])return 0;
-           }
-           else{
-             if(!dp[it]) return 0;
-           }
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dp[it] = dfs(adj, it, vis, dp);
+                if (!dp[it])
+                    return 0;
+            } else {
+                if (!dp[it])
+                    return 0;
+            }
         }
-        return dp[node]=cnt;
+        return dp[node] = cnt;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
         int n = adj.size();
-        vector<int>ans;
-        vector<int>vis(n,0),dp(n,0);
-        for(int i = 0;i<n;i++){
-            if(!vis[i]){
-                int res = dfs(adj,i,vis,dp);
+        vector<int> ans;
+        vector<int> vis(n, 0), dp(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                int res = dfs(adj, i, vis, dp);
             }
         }
-        for(int i = 0;i<n;i++)if(dp[i])ans.push_back(i);
+        for (int i = 0; i < n; i++)
+            if (dp[i])
+                ans.push_back(i);
         return ans;
     }
 };
