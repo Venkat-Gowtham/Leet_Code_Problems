9b48619cc6af9c9ebf0202c49b5aa4acce1afb2b
Gowtham Varri <21P31A0542@acet.ac.in>
2025-02-10 17:50:11 +0530

    Time: 5 ms (1.97%) | Memory: 11.1 MB (5.22%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        unordered_set<int>sett;
+        for(int i = 0 ; i < s.length(); i++){
+            if(s[i]>='a' and s[i]<='z') st.push(i);
+            else{
+                if(st.empty()) continue;
+                else {sett.insert(st.top());sett.insert(i);
+                st.pop();}
+            }
+        }
+       string res;
+       for(int i = 0;i<s.length();i++){
+        if(sett.find(i)==sett.end()){
+            cout<<i<<endl;
+            res+=s[i];
+        }
+       }
+       return res;
+    }
+};
