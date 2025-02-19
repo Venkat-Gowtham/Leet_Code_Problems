b4e4cd63a6efe80f2d0210daebac01cbdca25cec
Gowtham Varri <21P31A0542@acet.ac.in>
2025-02-19 12:05:03 +0530

    Time: 2622 ms (36.44%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,8 +1,41 @@
 # Write your MySQL query statement below
-
-
-select v.customer_id , count(v.visit_id ) as count_no_trans
+select customer_id , count(*) as count_no_trans
 from Visits v
-LEFT JOIN transactions t on v.visit_id = t.visit_id
-where t.visit_id is null 
-group by v.customer_id
+left join Transactions t
+on v.visit_id = t.visit_id
+where t.visit_id is null
+group by customer_id
+
+
+
+-- select branch , count(branch)
+-- from college
+-- join college2
+
+-- students
+-- student_id , name
+-- 1 divya
+-- 2 nandini
+-- 3 gowtham
+
+-- parents
+-- student_id,parent_name
+-- 1 tulasi
+-- 2 gowtam
+-- 3 tualsi
+-- 6 sindhu  grgiej
+
+
+
+-- student_id,name,student_id,parent_name
+-- 1          divya      1        tulasi
+-- 2          nandini    2        gowtham
+-- 3          gowtham    3        tulasi
+-- 4           hars      null     null 
+-- null        null      5         pr4mny 
+
+
+
+
+
+
